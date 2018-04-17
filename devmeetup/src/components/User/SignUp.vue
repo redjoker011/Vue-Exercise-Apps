55b87ed57d710aa2160a01d6314912e2d55246e9
Peter John Alvarado <redjoker011@gmail.com>
2018-04-17 14:28:16 +0800

    Update signin page

--- devmeetup/src/components/User/SignUp.vue
+++ devmeetup/src/components/User/SignUp.vue
@@ -107,47 +107,8 @@
         this.$store.dispatch('signUp', credentials)
       },
       onDismissed () {
-        console.log('Dismissed Alert')
         this.$store.commit('clearError')
       }
     }
   }
 </script>
-<style scoped>
-  .custom-loader {
-    animation: loader 1s infinite;
-    display: flex;
-  }
-  @-moz-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-  @-o-keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-  @keyframes loader {
-    from {
-      transform: rotate(0);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-</style>
